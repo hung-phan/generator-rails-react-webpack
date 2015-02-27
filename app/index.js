87d84256f1ed132a100e00791a7ff12117c0d369
hung-phan <colorvisavn@gmail.com>
2015-02-27 18:10:24 +0700

    Update templates

--- app/index.js
+++ app/index.js
@@ -152,6 +152,7 @@ module.exports = yeoman.generators.Base.extend({
     this.directory('webpack', 'config/webpack');
     this.copy('initializers/webpack.rb', 'config/initializers/webpack.rb');
     this.copy('tasks/webpack.rake', 'lib/tasks/webpack.rake');
+    this.copy('helpers/application_helper.rb', 'app/helpers/application_helper.rb');
     this.directory('__tests__', '__tests__');
   },
 
---
+++ app/templates/helpers/application_helper.rb
@@ -0,0 +1,21 @@
+module ApplicationHelper
+  def webpack_bundle_tag(bundle)
+    src =
+      if Rails.configuration.webpack[:use_manifest]
+        manifest = Rails.configuration.webpack[:asset_manifest][bundle]
+        bundle = manifest.instance_of?(Array) ? manifest[0] : manifest
+
+        "assets/#{bundle}"
+      else
+        "assets/build/#{bundle}.bundle"
+      end
+
+    "<script src='#{src}' type='text/javascript'></script>".html_safe
+  end
+
+  def webpack_manifest_script
+    return '' unless Rails.configuration.webpack[:use_manifest]
+    javascript_tag "window.webpackManifest = #{Rails.configuration.webpack[:common_manifest].to_json}"
+  end
+end
+
