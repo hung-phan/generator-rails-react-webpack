418c5ccf54ced5bd19acd74714ce31b1deed45ef
hung-phan <colorvisavn@gmail.com>
2015-04-19 17:11:13 +0700

    Fix url path for apis

--- app/index.js
+++ app/index.js
@@ -172,7 +172,7 @@ module.exports = yeoman.generators.Base.extend({
       var path   = 'config/routes.rb',
       hook   = 'Rails.application.routes.draw do\n',
       file   = this.readFileAsString(path),
-      insert = "  mount API::Base => '/api'\n";
+      insert = "  mount APIS::Base => '/api'\n";
 
       if (file.indexOf(insert) === -1) {
         this.write(path, file.replace(hook, hook + insert));
@@ -182,7 +182,7 @@ module.exports = yeoman.generators.Base.extend({
 
   grapeInitFile: function() {
     if (this.includeGrape) {
-      this.directory('api', 'app/api');
+      this.directory('apis', 'app/apis');
     }
   },
 
