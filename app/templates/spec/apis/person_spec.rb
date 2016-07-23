f226120469df4186695f2cb1b0b056b62d0d45dd
hung-phan <colorvisavn@gmail.com>
2016-07-23 13:54:07 +0700

    Update API

--- app/templates/spec/apis/person_spec.rb
+++ app/templates/spec/apis/person_spec.rb
@@ -5,26 +5,22 @@ describe 'APIS::V1::PersonApi' do
     it 'should get an array of people' do
       get '/api/v1/people'
 
-      expect_json_types :array
-      expect_json('0', {
-        firstName: 'John',
-        lastName: 'Smith',
-        age: 25,
-        address: {
-          streetAddress: '21 2nd Street',
-          city: 'New York',
-          state: 'NY',
-          postalCode: '10021'
+      result = JSON.parse(response.body)
+
+      expect result.is_a?(Array)
+      expect(result[0]).to eq({
+        'firstName' => 'John',
+        'lastName' => 'Smith',
+        'age' => 25,
+        'address' => {
+          'streetAddress' => '21 2nd Street',
+          'city' => 'New York',
+          'state' => 'NY',
+          'postalCode' => '10021'
         },
-        phoneNumber: [
-          {
-            type: 'home',
-            number: '212 555-1234'
-          },
-          {
-            type: 'fax',
-            number: '646 555-4567'
-          }
+        'phoneNumber'=>[
+          {'type' => 'home', 'number' => '212 555-1234'},
+          {'type' => 'fax', 'number' => '646 555-4567'}
         ]
       })
     end
